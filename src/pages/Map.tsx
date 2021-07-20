d7de8bde012058bab3504adef86c0b5b4f9f08f7
JulianRomana <julian.romana@hetic.net>
2021-07-20 16:34:24 +0200

    feat(map): add leaflet map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from 'react'
+import map from '../assets/map.svg'
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 const MapPage: FC = () => {
-  return <>Map page</>
+  return (
+    <MapContainer
+      center={[0, 0]}
+      zoom={1}
+      scrollWheelZoom={false}
+      className='h-screen'
+    >
+      <TileLayer url={map} tileSize={600} />
+    </MapContainer>
+  )
 }
 
 export default MapPage
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import WindiCSS from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), WindiCSS()],
+  optimizeDeps: {
+    entries: ['react', 'react-dom', 'react-router-dom', 'react-leaflet'],
+  },
 })
