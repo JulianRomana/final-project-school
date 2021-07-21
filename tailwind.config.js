2a67d321d4fde23b4c39f0bb35190c86bf1db8a4
Jacky <jacky.shao@hetic.net>
2021-07-21 15:24:10 +0200

    feat(theme): add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,39 @@ module.exports = {
     fontFamily: {
       sans: 'Work Sans, sans-serif',
     },
+    colors: {
+      blue: {
+        DEFAULT: '#0703D4',
+        75: '#0703d4bf',
+        50: '#0703d480',
+        25: '#0703d440',
+      },
+      orange: {
+        DEFAULT: '#ff4800',
+        75: '#ff4800bf',
+        50: '#ff480080',
+        25: '#ff480040',
+      },
+      yellow: {
+        DEFAULT: '#ffb600',
+        75: '#ffb600bf',
+        50: '#ffb60080',
+        25: '#ffb60040',
+      },
+      evergreen: {
+        DEFAULT: '#31d0aa',
+        75: '#31d0aabf',
+        50: '#31d0aa80',
+        25: '#31d0aa40',
+      },
+      onyx: '#0e0e2c',
+      stale: {
+        DEFAULT: '#4a4a68',
+        light: '#8c8ca1',
+      },
+      dorian: '#ecf1f4',
+      cloud: '#fafcfe',
+    },
     extend: {},
   },
   variants: {
